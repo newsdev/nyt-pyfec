7351140311012209d883def4a1d80cd141d99a6d
Jeremy Bowers <jeremyjbowers@gmail.com>
2015-09-08 15:19:11 -0400

    Gotta solve an issue with pip install and submodules.

--- setup.py
+++ setup.py
@@ -12,13 +12,15 @@ def read(filename):
 
 setup(
     name='nyt-pyfec',
-    version='0.0.4',
+    version='0.0.5',
     author='Jeremy Bowers',
     author_email='jeremy.bowers@nytimes.com',
     url='https://github.com/newsdev/nyt-pyfec',
     description="Python client for parsing campaign finance data from the Federal Election Commission's web site.",
     long_description=read('README.rst'),
     packages=['pyfec'],
+    package_dir={'pyfec': 'pyfec'},
+    package_data={'pyfec': ['fec-csv-sources/*.csv']},
     license="Apache License 2.0",
     keywords='FEC election finance campaign data parsing scraping donation expenditure candidate committee',
     install_requires=['lxml==3.4.4','psycopg2==2.6.1','python-dateutil==2.4.2','requests==2.7.0','six==1.9.0','wheel==0.24.0','colorama==0.3.3','humanize==0.5.1'],
