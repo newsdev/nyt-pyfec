5c39efd3404bfb5aa5567b55757a34cec0abfc23
Rachel Shorey <rshorey@gmail.com>
2015-10-19 17:02:02 -0400

    i am awesome at paths

--- pyfec/__init__.py
+++ pyfec/__init__.py
@@ -4,5 +4,5 @@ DELAY_TIME = int(os.environ.get("pyfec_delay_time", "1"))
 USER_AGENT = os.environ.get("pyfec_user_agent", "nyt-pyfec (jeremy.bowers@nytimes.com)")
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-CSV_FILE_DIRECTORY = 'pyfec/fec-csv-sources'
+CSV_FILE_DIRECTORY = '%s/pyfec/fec-csv-sources' % PROJECT_ROOT
 PAPER_CSV_FILE_DIRECTORY = '/tmp/paper_sources'
