86bfe4d42197de1d6c31c7dcac5fe330b35f329b
Rachel Shorey <rshorey@gmail.com>
2015-11-05 12:42:47 -0500

    added donations from comms & candidate

--- pyfec/filing.py
+++ pyfec/filing.py
@@ -306,6 +306,8 @@ def process_f3x_header(header_data):
         'new_loans':'total_loans',
         'tot_ies':'independent_expenditures',
         'tot_coordinated':'coordinated_expenditures_by_party_committees',
+        'tot_parties':'col_a_political_party_committees',
+        'tot_nonparty_comms':'col_a_other_political_committees_pacs',
         
         'outstanding_debts':'debts_by',
         'tot_contribs':'total_contributions',
@@ -323,7 +325,10 @@ def process_f3p_header(header_data):
         'new_loans':'total_loans',
         'tot_ies':'independent_expenditures',
         'tot_coordinated':'coordinated_expenditures_by_party_committees',
-        
+        'tot_parties':'col_a_political_party_committees',
+        'tot_nonparty_comms':'col_a_other_political_committees_pacs',
+        'tot_candidate':'col_a_the_candidate',
+
         'outstanding_debts':'debts_by',
         'tot_contribs':'total_contributions',
         'tot_ite_contribs_indivs':'individuals_itemized',
